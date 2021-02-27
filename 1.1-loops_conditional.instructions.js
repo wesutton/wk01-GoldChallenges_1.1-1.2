8a4dcedf597af25ae23c06ef8b5fca1a533acdae
Will Sutton <Wilazone@Wills-MacBook-Pro-3.local>
2021-02-27 11:46:37 -0500

    second

--- 1.1-loops_conditional.instructions.js
+++ 1.1-loops_conditional.instructions.js
@@ -8,3 +8,13 @@
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+
+for (let i = 0; i <=10; i++){
+    console.log(i);
+if(i % 2 === 0){
+    console.log('the number is even');
+}else {
+    console.log('the number is odd');
+}
+}
--- 1.2-arrays-objects.instructions.js
+++ 1.2-arrays-objects.instructions.js
@@ -7,3 +7,22 @@
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+let movie = {
+
+    nameOfMovie: "The Dark Knight",
+    runTime: "2hr 32min" ,
+    characters:[
+        {name: "Batman", age: 30, itemOne:["Batmobile", "Bruce Wayne"]},
+        {name: "Joker", age: 35, itemTwo:["Deck of Cards", "Jack Napier"]}
+
+    ],    
+    genre:"Action/Adventure",
+}
+
+console.log(movie.nameOfMovie);
+console.log(movie.runTime);
+console.log(movie.characters);
+console.log(movie.characters[0].name);
+console.log(movie.characters[1].itemTwo[0]);
+
